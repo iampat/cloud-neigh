f305425a2b35069be885ed6c19190ce5ac88b2e5
Ali Amiri <943064+iampat@users.noreply.github.com>
2023-03-20 12:37:52 -0700

    test and benchmark (#1)
    * test and benchmark
    
    * bazel
    
    * try to fix
    
    * both go and bazel builds
    
    * final change
    
    ---------
    
    Co-authored-by: Ali Amiri <ali@shooka-mac.local>

--- benchmark/benckmark_test.go
+++ benchmark/benckmark_test.go
@@ -19,24 +19,9 @@ func benchmarkFib(i int, b *testing.B) {
 	result = r
 }
 
+func BenchmarkFib2(b *testing.B)  { benchmarkFib(2, b) }
+func BenchmarkFib3(b *testing.B)  { benchmarkFib(3, b) }
 func BenchmarkFib10(b *testing.B) { benchmarkFib(10, b) }
+func BenchmarkFib20(b *testing.B) { benchmarkFib(20, b) }
+func BenchmarkFib40(b *testing.B) { benchmarkFib(40, b) }
 
-// func BenchmarkFib2(b *testing.B)  { benchmarkFib(2, b) }
-// func BenchmarkFib3(b *testing.B)  { benchmarkFib(3, b) }
-// func BenchmarkFib10(b *testing.B) { benchmarkFib(10, b) }
-// func BenchmarkFib20(b *testing.B) { benchmarkFib(20, b) }
-// func BenchmarkFib40(b *testing.B) { benchmarkFib(40, b) }
-
-// var result int
-
-// func BenchmarkFibComplete(b *testing.B) {
-// 	var r int
-// 	for n := 0; n < b.N; n++ {
-// 		// always record the result of Fib to prevent
-// 		// the compiler eliminating the function call.
-// 		r = Fib(10)
-// 	}
-// 	// always store the result to a package level variable
-// 	// so the compiler cannot eliminate the Benchmark itself.
-// 	result = r
-// }
