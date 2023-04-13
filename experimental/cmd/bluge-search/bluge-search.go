c8d3d4b173843a9de9c7ef4171a44d2a6b4b4e61
Ali Amiri <amiri1982@gmail.com>
2023-04-12 20:13:17 -0700

    read query from std in

--- experimental/cmd/bluge-search/bluge-search.go
+++ experimental/cmd/bluge-search/bluge-search.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"context"
+	"bufio"
+	"flag"
 	"fmt"
 	"log"
-	"time"
+	"os"
 
 	"github.com/blugelabs/bluge"
 )
 
 func main() {
-	// indexDir := "/Users/ali/workspace/data/bluge/index/" + "keep/wiki_en_full"
-	indexDir := "/Users/ali/workspace/data/bluge/index/keep/20220301_en_1k"
-	cfg := bluge.DefaultConfig(indexDir)
+	indexDir := flag.String("index", "/Users/ali/workspace/data/bluge/index/99p/", "where to load the data from")
+	flag.Parse()
+	cfg := bluge.DefaultConfig(*indexDir)
 	indexReader, err := bluge.OpenReader(cfg)
 	if err != nil {
 		log.Fatalf("unable to open reader: %v", err)
@@ -23,37 +24,50 @@ func main() {
 			log.Fatalf("error closing reader: %v", err)
 		}
 	}()
-
-	defer func(tStart time.Time) {
-		fmt.Println("Elapsed Time:", time.Since(tStart))
-	}(time.Now())
-
-	q := bluge.NewMatchQuery("alisp").SetField("title").SetFuzziness(1)
-
-	req := bluge.NewTopNSearch(10, q)
-
-	dmi, err := indexReader.Search(context.Background(), req)
-	if err != nil {
-		log.Fatalf("error executing search: %v", err)
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Println("Press Ctrl-D to exit.")
+	fmt.Printf("query:")
+	for scanner.Scan() {
+		query := scanner.Text()
+		fmt.Println("--------------------------------")
+		fmt.Println(query)
+		fmt.Println("--------------------------------")
+		fmt.Printf("query:")
 	}
+	if err := scanner.Err(); err != nil {
 
-	next, err := dmi.Next()
-	for err == nil && next != nil {
-		err = next.VisitStoredFields(func(field string, value []byte) bool {
-			if field == "title" {
-				fmt.Println(string(value))
-			}
+		log.Fatalln("oops!", err)
+	}
+	// defer func(tStart time.Time) {
+	// 	fmt.Println("Elapsed Time:", time.Since(tStart))
+	// }(time.Now())
 
-			return true
-		})
-		if err != nil {
-			log.Fatalf("error accessing stored fields: %v", err)
-		}
-		fmt.Println("----------------------------------")
-		next, err = dmi.Next()
-	}
-	if err != nil {
-		log.Fatalf("error iterating results: %v", err)
-	}
+	// q := bluge.NewMatchQuery("pto").SetField("title").SetFuzziness(1)
+
+	// req := bluge.NewTopNSearch(10, q)
+
+	// dmi, err := indexReader.Search(context.Background(), req)
+	// if err != nil {
+	// 	log.Fatalf("error executing search: %v", err)
+	// }
+
+	// next, err := dmi.Next()
+	// for err == nil && next != nil {
+	// 	err = next.VisitStoredFields(func(field string, value []byte) bool {
+	// 		if field == "title" {
+	// 			fmt.Println(string(value))
+	// 		}
+	// 		fmt.Println(field)
+	// 		return true
+	// 	})
+	// 	if err != nil {
+	// 		log.Fatalf("error accessing stored fields: %v", err)
+	// 	}
+	// 	fmt.Println("----------------------------------")
+	// 	next, err = dmi.Next()
+	// }
+	// if err != nil {
+	// 	log.Fatalf("error iterating results: %v", err)
+	// }
 
 }
